4c548682e89d48a9424aa4b5c1b674d9a15285f7
angelitorogo <angelitorogo@hotmail.com>
2026-04-06 11:17:15 +0200

    sin_env

---
+++ src/environments/environment-example.ts
@@ -0,0 +1,11 @@
+export const environment_example = {
+    API_URL: '',
+    DOMAIN_URL: '',
+    APP_NAME: '',
+    PRODUCTION: false,
+    TOKEN: "",
+    KEY_RCAPTCHA: "",
+    ADSCLIENTID: "",
+    GAMEASUREMENTID: '',
+    API_KEY_GOOGLEMAPS: ''
+};
